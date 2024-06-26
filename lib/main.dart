27486ab445c248399c632441c1a8d594bd1c1b10
machine <brainychip@gmail.com>
2024-06-26 01:01:20 +0000

    Ben: center widget... baby steps

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,46 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Calculator(title: 'Calculator App'),
     );
   }
 }
+
+class Calculator extends StatefulWidget {
+  final String title;
+
+  const Calculator({
+    super.key, required this.title,
+  });
+
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calculator"),
+      ),
+      body: const Center(
+
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Welcome"),
+          Text("Welcome"),
+
+        ],
+      )
+      )
+    );
+  }
+
+}
+
+
+
